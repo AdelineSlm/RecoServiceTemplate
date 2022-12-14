90edc8bd90d2d1c4c8eb51d735da18ea9cc00011
AdelineSlm <easterlywi18@gmail.com>
2022-12-14 10:12:06 +0300

    Loading a pretrained model

--- service/load_data.py
+++ service/load_data.py
@@ -3,6 +3,9 @@ import gdown
 url = "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1W2ckIrN7KKiMZbF00hfH0rFSTKdmPZnr"
 gdown.download(url, "../service/pretrained_models/tfidf_15.dill")
 
+url = "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1arIwVzYtxFJgKff8gkZWB43-1U5CfXRv"
+gdown.download(url, "../service/pretrained_models/lightfm.dill")
+
 url = "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1-5yCqSkSO-FECtoWlg2DdtsbUpRQy6qu"
 gdown.download(url, "../service/data/genres_dict.dill")
 
